Either next_iteration() assings no data to validation partition, or the num_batches calculation is wrong

generator.py
@@ -3,7 +3,7 @@ Tensorflow implementation of methods for generating RNN output.
 
 Copyright (c) 2017 Frank Derry Wanye
 
-Date: 24 September, 2017
+Date: 27 September, 2017
 """
 
 import numpy as np
@@ -17,9 +17,9 @@ def generate_output(model, num_tokens=30, num_outputs=10):
     """
     Generates output from the RNN.
 
-    Params: 
+    Params:
     model (rnn.RNNModel): The model to be used to generate output
-    num_tokens (int): The number of tokens to generate. If set to infinity, the output is generated until an END token 
+    num_tokens (int): The number of tokens to generate. If set to infinity, the output is generated until an END token
                       is reached
     num_outputs (int): The number of outputs to generate.
 
@@ -30,7 +30,7 @@ def generate_output(model, num_tokens=30, num_outputs=10):
     outputs = []
     for i in range(1, num_outputs+1):
         sentence = generate_single_output(model, num_tokens)
-        sentence = " ".join([model.index_to_token[word] for word in sentence])
+        sentence = " ".join([model.dataset.index_to_token[word] for word in sentence])
         outputs.append(sentence)
     for sentence in outputs:
         print("%s\n\n" % sentence)
@@ -42,21 +42,21 @@ def generate_single_output(model, num_tokens=math.inf):
     """
     Generates a single output from the model.
 
-    Params: 
+    Params:
     model (rnn.RNNModel): The model to be used to generate output
-    num_tokens (int): The number of tokens to generate. If set to infinity, the output is generated until an END token 
+    num_tokens (int): The number of tokens to generate. If set to infinity, the output is generated until an END token
                       is reached
 
     Return:
     np.ndarray: The generated output as a numpy array
     """
-    sentence = np.array([model.token_to_index[constants.START_TOKEN]])
+    sentence = np.array([model.dataset.token_to_index[constants.START_TOKEN]])
     current_state = np.zeros(tuple(model.hidden_state_shape))
     num_generated = 0
     while num_tokens > num_generated:
         output, new_current_state = predict(model, sentence, current_state)
         sentence = np.append(sentence, sample_output_token(model, output))
-        if sentence[-1] == model.token_to_index[constants.END_TOKEN] : break
+        if sentence[-1] == model.dataset.token_to_index[constants.END_TOKEN] : break
         num_generated += 1
     # End of while loop
     return sentence
@@ -82,11 +82,11 @@ def predict(model, sentence, current_state):
     sizes = [model.settings.train.truncate] * model.settings.train.batch_size
 
     predictions, final_hidden_state = model.session.run(
-        [model.predictions_series, model.current_state], 
+        [model.predictions_series, model.current_state],
         feed_dict={
-            model.batch_x_placeholder:input_batch, 
+            model.batch_x_placeholder:input_batch,
             model.batch_sizes:sizes,
-            model.hidden_state_placeholder:current_state   
+            model.hidden_state_placeholder:current_state
         })
 
     position = (len(sentence)-1) % model.settings.train.truncate
@@ -97,7 +97,7 @@ def sentence_to_batch_array(train_settings, sentence):
     """
     Convert sentence to batch array.
 
-    Params: 
+    Params:
     train_settings (settings.SettingsNamespace): The training settings
     sentence (list): The input sentence as a list of integers
 
@@ -115,7 +115,7 @@ def sentence_to_batch_array(train_settings, sentence):
 
 def sample_output_token(model, probabilities):
     """
-    Returns the probable next word in sentence. Some randomization is included to make sure 
+    Returns the probable next word in sentence. Some randomization is included to make sure
     that not all the sentences produced are the same.
 
     :type model: RNNModel()
@@ -130,7 +130,7 @@ def sample_output_token(model, probabilities):
     return np.argmax(probabilities[:-1])
     # output_word = model.token_to_index[constants.UNKNOWN]
     # while output_word == model.token_to_index[constants.UNKNOWN]:
-    #     while sum(probabilities[:-1]) > 1.0 : 
+    #     while sum(probabilities[:-1]) > 1.0 :
     #         model.logger.error("Sum of word probabilities (%f) > 1.0" % sum(probabilities[:-1]))
     #         probabilities = softmax(probabilities)
     #     samples = np.random.multinomial(1, probabilities)
@@ -141,7 +141,7 @@ def sample_output_token(model, probabilities):
 def softmax(probabilities):
     """
     Compute softmax values for each sets of scores in the given array.
-    It is needed because python's floats are dumb. 
+    It is needed because python's floats are dumb.
     There are times when, say, 1.00000 does not equal 1
     Credit: https://stackoverflow.com/q/34968722/5760608
 

model.py
@@ -37,7 +37,7 @@ class RNNModel(object):
         self.model_path = saver.create_model_dir(self.settings.general.model_name)
         self.logger = setup.setup_logger(self.settings.logging, self.model_path)
         self.logger.info("RNN settings: %s" % self.settings)
-        self.dataset = Dataset(self.logger, self.settings.rnn.dataset, self.settings.train)
+        self.dataset = dataset.Dataset(self.logger, self.settings.rnn.dataset, self.settings.train)
         self.create_graph()
     # End of __init__()
 
@@ -91,11 +91,11 @@ class RNNModel(object):
                 shape=np.shape(self.batch_x_placeholder),
                 name="output_placeholder")
             self.out_weights = tf.Variable(
-                initial_value=np.random.rand(self.settings.rnn.hidden_size, self.vocabulary_size),
+                initial_value=np.random.rand(self.settings.rnn.hidden_size, self.dataset.vocabulary_size),
                 dtype=tf.float32,
                 name="out_weights")
             self.out_bias = tf.Variable(
-                np.zeros((self.vocabulary_size)),
+                np.zeros((self.dataset.vocabulary_size)),
                 dtype=tf.float32,
                 name="out_bias")
             logits_series = [
@@ -136,8 +136,8 @@ class RNNModel(object):
                 dtype=tf.int32,
                 shape=[self.settings.train.batch_size, self.settings.train.truncate],
                 name="input_placeholder")
-            if self.data_type == constants.TYPE_CHOICES[0]: # data type = 'text'
-                inputs_series = token_to_vector(self.vocabulary_size, self.settings.rnn.hidden_size,
+            if self.dataset.data_type == constants.TYPE_CHOICES[0]: # data type = 'text'
+                inputs_series = token_to_vector(self.dataset.vocabulary_size, self.settings.rnn.hidden_size,
                     self.batch_x_placeholder)
             else:
                 print("ERROR: Numeric inputs cannot be handled yet.")

trainer.py
@@ -28,9 +28,8 @@ def train(model):
     # writer = tf.summary.FileWriter(model.model_path + "tensorboard", graph=model.session.graph)
     loss_list = []
 
-    current_state = np.zeros((model.settings.train.batch_size, model.settings.rnn.hidden_size), dtype=float)
     for epoch_num in range(1, model.settings.train.epochs + 1):
-        average_loss, current_state = train_epoch(model, epoch_num, current_state)
+        average_loss = train_epoch(model, epoch_num)
         loss_list.append(average_loss)
         # End of epoch training
 
@@ -38,14 +37,13 @@ def train(model):
     plot(model, loss_list)
 # End of train()
 
-def train_epoch(model, epoch_num, current_state):
+def train_epoch(model, epoch_num):
     """
     Trains one full epoch.
 
     Params:
     model (model.RNNModel): The model to train
     epoch_num (int): The number of the current epoch
-    current_state (np.ndarray): The current state of the hidden layer
 
     Return:
     average_loss (float): The average incurred loss
@@ -53,50 +51,109 @@ def train_epoch(model, epoch_num, current_state):
     """
     model.logger.info("Starting epoch: %d" % (epoch_num))
 
-    '''
-    Workflow:
-    while(next_iteration):
-        for batch in train_batches
-            train()
-        for batch in valid_batches
-            validate()
-    '''
+    cross_validation_loss = 0
+    current_state = np.zeros((model.settings.train.batch_size, model.settings.rnn.hidden_size), dtype=float)
+    for section in range(model.dataset.num_sections):
+        model.dataset.next_iteration()
+        train_step(model, epoch_num, current_state)
+        minibatch_loss = validation_step(model, epoch_num, current_state)
+        cross_validation_loss += minibatch_loss
 
-    while model.dataset.next_iteration() is True:
-        
-    total_loss = 0
-    for batch_num in range(model.num_batches):
-        # Debug log outside of function to reduce number of arguments.
-        model.logger.debug("Training minibatch : ", batch_num, " | ", "epoch : ", epoch_num + 1)
-        minibatch_loss, current_state = train_minibatch(model, batch_num, current_state)
-        total_loss += minibatch_loss
-    # End of batch training
-    average_loss = total_loss / model.num_batches
-    model.logger.info("Finished epoch: %d | loss: %f" % (epoch_num, average_loss))
-    return average_loss, current_state
+    cross_validation_loss /= model.dataset.num_sections
+    model.logger.info("Finished epoch: %d | loss: %f" % (epoch_num, cross_validation_loss))
+    return cross_validation_loss
 # End of train_epoch()
+
+def train_step(model, epoch_num, current_state):
+    """
+    Trains the model on the dataset's training partition.
+
+    Params:
+    model (model.RNNModel): The model to train
+    epoch_num (int): The number of the current epoch
+    current_state (np.ndarray): The current state of the hidden layer
+    """
+    for batch_num in range(model.dataset.train.num_batches):
+        # Debug log outside of function to reduce number of arguments.
+        model.logger.debug("Training minibatch : ", batch_num, " | ", "epoch : ", epoch_num)
+        current_state = train_minibatch(model, batch_num, current_state)
+# End of train_step()
 
 def train_minibatch(model, batch_num, current_state):
     """
     Trains one minibatch.
 
     Params:
-    model (rnn.RNNModel): The model to train
+    model (model.RNNModel): The model to train
     batch_num (int): The current batch number
     current_state (np.ndarray): The current hidden state of the model
 
     Return:
-    tuple: (minibatch_loss, updated_hidden_state)
+    updated_hidden_state (np.ndarray): The updated state of the hidden layer after training
     """
-    batch_x = model.inputs[batch_num]
-    batch_y = model.labels[batch_num]
-    sizes = model.sizes[batch_num]
+    batch_x = model.dataset.train.x[batch_num]
+    batch_y = model.dataset.train.y[batch_num]
+    sizes = model.dataset.train.sizes[batch_num]
 
-    if batch_x[0][0] == model.token_to_index[constants.START_TOKEN]: # Reset state if start of sentence
+    if batch_x[0][0] == model.dataset.token_to_index[constants.START_TOKEN]: # Reset state if start of example
         current_state = np.zeros(tuple(model.hidden_state_shape), dtype=float)
 
-    total_loss, train_step, current_state, summary = model.session.run(
-        [model.total_loss_op, model.train_step_fun, model.current_state, model.summary_ops],
+    train_step, current_state = model.session.run(
+        [model.train_step_fun, model.current_state],
+        feed_dict={
+            model.batch_x_placeholder:batch_x,
+            model.batch_y_placeholder:batch_y,
+            model.batch_sizes:sizes,
+            model.hidden_state_placeholder:current_state
+        })
+
+    return current_state
+# End of train_minibatch()
+
+def validation_step(model, epoch_num, current_state):
+    """
+    Performs performance calculations on the dataset's validation partition.
+
+    Params:
+    model (model.RNNModel): The model to train
+    epoch_num (int): The number of the current epoch
+    current_state (np.ndarray): The current state of the hidden layer
+
+    Return:
+    average_loss (float): The average loss over all minibatches in the validation partition
+    """
+    total_validation_loss = 0
+    for batch_num in range(model.dataset.valid.num_batches):
+        # Debug log outside of function to reduce number of arguments.
+        model.logger.debug("Validating minibatch : ", batch_num, " | ", "epoch : ", epoch_num)
+        minibatch_loss, current_state = validate_minibatch(model, batch_num, current_state)
+        total_validation_loss += minibatch_loss
+    average_validation_loss = total_validation_loss / model.dataset.valid.num_batches
+    return average_validation_loss
+# End of validation_step()
+
+def validate_minibatch(model, batch_num, current_state):
+    """
+    Calculates the performance of the network on one minibatch, logs the performance to tensorflow.
+
+    Params:
+    model (model.RNNModel): The model to train
+    batch_num (int): The current batch number
+    current_state (np.ndarray): The current hidden state of the model
+
+    Return:
+    minibatch_loss (float): The average loss over this minibatch
+    updated_hidden_state (np.ndarray): The updated state of the hidden layer after training
+    """
+    batch_x = model.dataset.train.x[batch_num]
+    batch_y = model.dataset.train.y[batch_num]
+    sizes = model.dataset.train.sizes[batch_num]
+
+    if batch_x[0][0] == model.dataset.token_to_index[constants.START_TOKEN]: # Reset state if start of example
+        current_state = np.zeros(tuple(model.hidden_state_shape), dtype=float)
+
+    total_loss, current_state, summary = model.session.run(
+        [model.total_loss_op, model.current_state, model.summary_ops],
         feed_dict={
             model.batch_x_placeholder:batch_x,
             model.batch_y_placeholder:batch_y,
@@ -105,9 +162,8 @@ def train_minibatch(model, batch_num, current_state):
         })
 
     model.summary_writer.add_summary(summary)
-
     return total_loss, current_state
-# End of train_minibatch()
+# End of validate_minibatch()
 
 def plot(model, loss_list):
     """