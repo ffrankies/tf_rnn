694fbbefef1840eca92981deb6de58b363c02668
Frank Wanye <wanyef@mail.gvsu.edu>
2018-05-21 13:10:00 +0000

    Marked private methods

--- layers/performance_layer.py
+++ layers/performance_layer.py
@@ -1,6 +1,6 @@
 '''
 Contains functions for setting up the performance evaluation layer for a tensorflow-based RNN.
-@since 0.4.1
+@since 0.4.4
 '''
 import tensorflow as tf
 import numpy as np
@@ -82,7 +82,7 @@ class Accumulator(object):
         self.latest_timestep_accuracies = list()
         self.next_timestep_accuracies = list()
         self.next_timestep_elements = list()
-        self.reset_metrics()
+        self._reset_metrics()
     # End of __init__()
 
     def update(self, data, beginning, ending):
@@ -99,21 +99,21 @@ class Accumulator(object):
           - labels (list): The correct predictions for the minibatch
           - sequence_lengths (list): The lengths of each sequence in the minibatch
         - beginning (boolean): True if this minibatch marks the start of a sequence
-        - ending (boolean): True if this minibatch maarks the end of a sequence
+        - ending (boolean): True if this minibatch marks the end of a sequence
         """
         loss, accuracy, size, timestep_accuracies, timestep_elements, predictions, labels, sequence_lengths = data
         # self.logger.debug("Minibatch loss: %.2f | Minibatch accuracy: %.2f" % (loss, accuracy))
-        self.loss = self.update_average(self.loss, self.elements, loss, size)
-        self.accuracy = self.update_average(self.accuracy, self.elements, accuracy, size)
+        self.loss = self._update_average(self.loss, self.elements, loss, size)
+        self.accuracy = self._update_average(self.accuracy, self.elements, accuracy, size)
         self.elements += size
         self.extend_timesteps(timestep_accuracies, timestep_elements)
         if ending is True:
-            self.merge_timesteps()
+            self._merge_timesteps()
         self.confusion_matrix.update(predictions, labels, sequence_lengths)
         # self.logger.debug("Updated loss: %.2f | Updated accuracy: %.2f" % (self.loss, self.accuracy))
     # End of update()
 
-    def update_average(self, old_avg, old_num, new_avg, new_num):
+    def _update_average(self, old_avg, old_num, new_avg, new_num):
         """Updates the old average with new data.
 
         Params:
@@ -128,7 +128,7 @@ class Accumulator(object):
         updated_num = old_num + new_num
         updated_avg = updated_sum / updated_num
         return updated_avg
-    # End of update_average()
+    # End of _update_average()
 
     def extend_timesteps(self, accuracies, sizes):
         """Appends the timestep accuracies and timestep sizes to the next_timestep_elements list.
@@ -147,20 +147,20 @@ class Accumulator(object):
         self.next_timestep_elements.extend(sizes)
     # End of extend_timesteps()
 
-    def merge_timesteps(self):
+    def _merge_timesteps(self):
         """Updates the cumulative timestep accuracies.
         """
         timestep_accuracies = self.next_timestep_accuracies[:self.max_sequence_length]
         timestep_elements = self.next_timestep_elements[:self.max_sequence_length]
         if not self.timestep_accuracies:
-            self.copy_timestep_accuracy_info(timestep_accuracies, timestep_elements)
+            self._copy_timestep_accuracy_info(timestep_accuracies, timestep_elements)
         else:
-            self.update_timestep_accuracy_info(timestep_accuracies, timestep_elements)
+            self._update_timestep_accuracy_info(timestep_accuracies, timestep_elements)
         self.next_timestep_accuracies = list()
         self.next_timestep_elements = list()
-    # End of merge_timesteps()
+    # End of _merge_timesteps()
 
-    def copy_timestep_accuracy_info(self, timestep_accuracies: list, timestep_elements: list):
+    def _copy_timestep_accuracy_info(self, timestep_accuracies: list, timestep_elements: list):
         """Copies over the next timestep accuracy and elements info into the timestep_accuracies and timestep_elements
         variables.
 
@@ -170,9 +170,9 @@ class Accumulator(object):
         """
         self.timestep_accuracies = deepcopy(timestep_accuracies)
         self.timestep_elements = deepcopy(timestep_elements)
-    # End of copy_timestep_accuracy_info()
+    # End of _copy_timestep_accuracy_info()
 
-    def update_timestep_accuracy_info(self, timestep_accuracies: list, timestep_elements: list):
+    def _update_timestep_accuracy_info(self, timestep_accuracies: list, timestep_elements: list):
         """Updates the timestep accuracy information by keeping a running average over all the data presented.
 
         Params:
@@ -184,9 +184,9 @@ class Accumulator(object):
             old_num = self.timestep_elements[index]
             new_avg = timestep_accuracies[index]
             new_num = timestep_elements[index]
-            self.timestep_accuracies[index] = self.update_average(old_avg, old_num, new_avg, new_num)
+            self.timestep_accuracies[index] = self._update_average(old_avg, old_num, new_avg, new_num)
             self.timestep_elements[index] += new_num
-    # End of update_timestep_accuracy_info()
+    # End of _update_timestep_accuracy_info()
 
     def next_epoch(self):
         """Creates space for storing performance data for the next epoch. Also resets metrics for the next epoch.
@@ -199,10 +199,10 @@ class Accumulator(object):
         self.losses.append(self.loss)
         self.accuracies.append(self.accuracy)
         self.latest_timestep_accuracies = deepcopy(self.timestep_accuracies)
-        self.reset_metrics()
+        self._reset_metrics()
     # End of next_epoch()
 
-    def reset_metrics(self):
+    def _reset_metrics(self):
         """Resets the performance metrics for the next epoch.
 
         Creates the following instance variables, if they haven't already been created:
@@ -219,7 +219,7 @@ class Accumulator(object):
         self.timestep_elements = None
         self.latest_confusion_matrix = self.confusion_matrix.copy()
         self.confusion_matrix = ConfusionMatrix(self.logger)
-    # End of reset_metrics()
+    # End of _reset_metrics()
 # End of PerformanceData()
 
 class ConfusionMatrix(object):
