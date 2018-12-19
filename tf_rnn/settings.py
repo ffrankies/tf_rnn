a359c553cad593986ddf90d217690988e9b5d8af
Frank Wanye <wanyef@mail.gvsu.edu>
2018-12-18 22:16:11 -0500

    Type hinting and return param formatting

--- tf_rnn/settings.py
+++ tf_rnn/settings.py
@@ -2,7 +2,9 @@
 
 @since 0.6.3
 """
-import argparse  # I want to see if I can get rid of this - it's only use is type hinting
+import argparse
+from collections import Iterable
+from typing import Dict, Tuple, Any
 
 import yaml
 
@@ -15,14 +17,15 @@ class SettingsNamespace(object):
     """An extensible Namespace object for easy accessibility to inner variables.
     """
 
-    def __init__(self, parameters: dict = None):
+    def __init__(self, parameters: Dict = None) -> None:
         """Creates a SettingsNamespace out of the given dictionary object.
 
         Params:
-        - parameters (dict[str, Any]): The parameters with which to update the settings namespace. If none are given,
+            parameters (Dict[str, Any]): The parameters with which to update the settings namespace. If none are given,
                                        does nothing.
         """
-        self.update(parameters)
+        if parameters:
+            self.update(parameters)
     # End of __init__()
 
     def __str__(self):
@@ -32,12 +35,12 @@ class SettingsNamespace(object):
         return "{}: {}".format(self.__class__.__name__, self.__dict__)
     # End of __str__()
 
-    def update(self, parameters: dict):
+    def update(self, parameters: Dict):
         """Updates the class parameters with the given ones. Overwrites any existing parameters with newly provided
         ones. Any class parameters not present in the given dictionary will be left as is.
 
         Params:
-        - parameters (dict[str, Any]): The parameters with which to update the settings namespace. If none are given,
+            parameters (Dict[str, Any]): The parameters with which to update the settings namespace. If none are given,
                                        does nothing.
         """
         if parameters:
@@ -50,14 +53,14 @@ class GeneralSettings(SettingsNamespace):
     """A namespace object for the general RNN settings.
     """
 
-    def __init__(self, parameters: dict = None):
+    def __init__(self, parameters: Dict = None) -> None:
         """Creates default parameters for general settings, and then updates them with the given dictionary.
         
         TODO:
         - Change parameters names to better ones
         
         Params:
-        - parameters (dict[str, Any]): The parameters with which to update the general settings 
+            parameters (Dict[str, Any]): The parameters with which to update the general settings 
         """
         self.model_name = constants.MODEL_NAME
         self.new_model = constants.NEW_MODEL  # TODO: rename
@@ -71,14 +74,14 @@ class LoggingSettings(SettingsNamespace):
     """A namespace object for storing the settings for the RNN's logger.
     """
 
-    def __init__(self, parameters: dict = None):
+    def __init__(self, parameters: Dict = None) -> None:
         """Creates default parameters for logging settings, and then updates them with the given dictionary.
 
         TODO:
         - Get rid of unused logging parameters
 
         Params:
-        - parameters (dict[str, Any]): The parameters with which to update the logging settings 
+            parameters (Dict[str, Any]): The parameters with which to update the logging settings 
         """
         self.log_name = constants.LOG_NAME  # TODO: possibly remove
         self.log_dir = constants.LOG_DIR  # TODO: possibly remove
@@ -93,14 +96,14 @@ class RNNSettings(SettingsNamespace):
     """A namespace object for storing the RNN settings.
     """
 
-    def __init__(self, parameters: dict = None):
+    def __init__(self, parameters: Dict = None) -> None:
         """Creates default parameters for the RNN settings, and then updates them with the given dictionary.
 
         TODO:
         - Get rid of unused RNN logging parameters
 
         Params:
-        - parameters (dict[str, Any]): The parameters with which to update the RNN settings 
+            parameters (Dict[str, Any]): The parameters with which to update the RNN settings 
         """
         self.dataset = constants.DATASET
         self.embed_size = constants.EMBED_SIZE
@@ -118,14 +121,14 @@ class TrainingSettings(SettingsNamespace):
     """A namespace object for RNN training settings.
     """
 
-    def __init__(self, parameters: dict = None):
+    def __init__(self, parameters: Dict = None) -> None:
         """Creates default parameters for RNN training settings, and then updates them with the given dictionary.
 
         TODO:
         - Get rid of unused training parameters
 
         Params:
-        - parameters (dict[str, Any]): The parameters with which to update the training settings 
+            parameters (Dict[str, Any]): The parameters with which to update the training settings 
         """
         self.batch_size = constants.BATCH_SIZE
         self.patience = constants.PATIENCE
@@ -143,7 +146,7 @@ class DatasetSettings(SettingsNamespace):
     """A namespace object for logging dataset settings.
     """
 
-    def __init__(self, parameters: dict = None):
+    def __init__(self, parameters: Dict = None) -> None:
         """Creates default parameters for dataset settings, and then updates them with the given dictionary.
 
         TODO:
@@ -151,7 +154,7 @@ class DatasetSettings(SettingsNamespace):
         - Figure out why the config file is one of the dataset settings
 
         Params:
-        - parameters (dict[str, Any]): The parameters with which to update the dataset settings 
+            parameters (Dict[str, Any]): The parameters with which to update the dataset settings 
         """
         self.config_file = constants.CONFIG_FILE  # Why is this here?
         self.raw_data = constants.RAW_DATA
@@ -180,14 +183,14 @@ class Settings(object, metaclass=Singleton):
     The object is a singleton to ensure that only one instance of settings is ever active.
     """
 
-    def __init__(self, dataset_only: bool = False):
+    def __init__(self, dataset_only: bool = False) -> None:
         """Creates the Settings class.
         The class is created from the provided config file if it is supplied.
         If 'options' is passed in as a command-line argument, then the class is created from the command-line
         arguments passed into it.
 
         Params:
-        - dataset_only (bool): True if only settings for the dataset should be provided
+            dataset_only (bool): True if only settings for the dataset should be provided
         """
         # Set up subsettings with default parameters
         self.general = GeneralSettings()
@@ -208,20 +211,20 @@ class Settings(object, metaclass=Singleton):
         line.
 
         Return:
-        - settings_string (str): A string representation of the Namespaces comprising this Settings object.
+            str: A string representation of the Namespaces comprising this Settings object.
         """
         subsettings = [self.general, self.logging, self.rnn, self.train, self.data]
         return "{}: \n\t{}".format(self.__class__.__name__, "\n\t".join(map(str, subsettings)))
     # End of __str__()
 
-    def _get_config_dicts(self, dataset_only: bool) -> tuple:
+    def _get_config_dicts(self, dataset_only: bool) -> Tuple:
         """Obtains the configuration dictionaries from either the config file or the command-line arguments.
 
         Params:
-        - dataset_only (bool): True if only settings for the dataset should be provided
+            dataset_only (bool): True if only settings for the dataset should be provided
 
         Return:
-        - config_dicts (tuple): Separate dictionaries for each configuration category.
+            Tuple: Separate dictionaries for each configuration category.
         """
         args = cmd_arg_parser.parse_arguments(dataset_only)
         if 'config_file' in args and args.config_file is not None:
@@ -231,14 +234,18 @@ class Settings(object, metaclass=Singleton):
         return config_dicts
     # End of _get_config_dicts()
 
-    def _parse_config_yml(self, config_file: str) -> str:
+    def _parse_config_yml(self, config_file: str) -> Tuple:
         """Parses the YAML config file into multiple dictionaries.
 
         Params:
-        - config_file (string): The path to the config file from which to load settings
+            config_file (str): The path to the config file from which to load settings
 
         Return:
-        - config_dicts (tuple<dict>): A list of dictionaries, each representing a different section of the settings
+            Dict[str, Any]: The general settings
+            Dict[str, Any]: The logging settings
+            Dict[str, Any]: The RNN/model settings
+            Dict[str, Any]: The training settings
+            Dict[str, Any]: The dataset settings
         """
         yaml_settings = self._read_yml(config_file)
         # Break settings into categories
@@ -254,43 +261,47 @@ class Settings(object, metaclass=Singleton):
         """Reads the contents of a YAML file and returns the file contents as a dictionary.
 
         Params:
-        - yml_file (string): The path to the YAML file
+            yml_file (string): The path to the YAML file
 
         Return:
-        - yml_contents (dict<string, any>): A dictionary containing the contents of the YAML file
+            Dict[str, Any]: A dictionary containing the contents of the YAML file
         """
         with open(yml_file, 'r') as stream:
             yml_contents = yaml.safe_load(stream)
         return yml_contents
     # End of _read_yml()
 
-    def _parse_config_args(self, args: argparse.Namespace) -> tuple:
+    def _parse_config_args(self, args: argparse.Namespace) -> Tuple:
         """Parses the contents of the command-line arguments into multiple dictionaries.
 
         Params:
-        - args (argparse.Namespace): The command-line arguments passed in to the program
+            args (argparse.Namespace): The command-line arguments passed in to the program
 
         Return:
-        - arg_dictionaries (tuple<dict>): A list of dictionaries, each representing a different section of the settings
+            Dict[str, Any]: The general settings
+            Dict[str, Any]: The logging settings
+            Dict[str, Any]: The RNN/model settings
+            Dict[str, Any]: The training settings
+            Dict[str, Any]: The dataset settings
         """
         # Create dicts for individual categories
-        general = self.get_arg_subset(args, vars(self.general).keys())
-        logging = self.get_arg_subset(args, vars(self.logging).keys())
-        rnn = self.get_arg_subset(args, vars(self.rnn).keys())
-        train = self.get_arg_subset(args, vars(self.train).keys())
-        data = self.get_arg_subset(args, vars(self.data).keys())
+        general = self._get_arg_subset(args, vars(self.general).keys())
+        logging = self._get_arg_subset(args, vars(self.logging).keys())
+        rnn = self._get_arg_subset(args, vars(self.rnn).keys())
+        train = self._get_arg_subset(args, vars(self.train).keys())
+        data = self._get_arg_subset(args, vars(self.data).keys())
         return general, logging, rnn, train, data
     # End of _parse_config_args()
 
-    def _get_arg_subset(self, args: argparse.Namespace, arg_keys: list) -> dict:
+    def _get_arg_subset(self, args: argparse.Namespace, arg_keys: Iterable) -> Dict[str, Any]:
         """Creates a dictionary containing the given keys from the command_line args.
 
         Params:
-        - args (argparse.Namespace): The command-line arguments passed in to the program
-        - arg_keys (list<str>): The keys that form this argument group
+            args (argparse.Namespace): The command-line arguments passed in to the program
+            arg_keys (list<str>): The keys that form this argument group
 
         Return:
-        - arg_subset(dict<string, any>): The general settings passed into the program
+            Dict[str, Any]: The general settings passed into the program
         """
         arg_dict = vars(args)
         new_dict = dict()
